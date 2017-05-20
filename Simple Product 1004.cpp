f1e5291c20c0e76585ae38bf7ceaa7ac4b9c7b71
MDALAMIN10 <aamin5044@gmail.com>
2017-05-20 14:24:48 +0600

    Initial Commit

---
+++ Simple Product 1004.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B ;                     //scanf("%d%d", &A,&B);
+    cout << "PROD = " << A * B << endl; // printf("PROD = %d\n", A*B);
+    return 0;
+}
